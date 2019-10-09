c00321d6ba5afa855ba98c84f9d27fb2e23077c8
stenixon0 <50377151+stenixon0@users.noreply.github.com>
2019-10-09 16:45:06 -0400

    Create projectTwo.java

---
+++ projectTwo.java
@@ -0,0 +1,54 @@
+// setup gui
+var Options = function() {
+};
+
+var gui = new dat.GUI();
+var ops = new Options();
+
+let squares = [];
+let series = [];
+let gratio = 1.61803;
+let startingvalue = 500;
+
+
+function setup () {
+  for(i = 0; i < 20; i++){
+    series[i] = startingvalue / pow(gratio, i);
+    squares[i] = new Square(0, 0, series[i]);
+  }
+  createCanvas(2 *(series[0] + series[1]),
+              2 * series[0]);
+  
+  
+}
+
+function draw() {
+  background(0);
+  noFill();
+  stroke(255); 
+  
+  push();
+  squares[0].display();
+  for(i = 1; i < squares.length; i++){
+    translate(series[i - 1] + series[i], 0);
+    rotate(HALF_PI);
+    squares[i].display();
+  }
+  pop();
+}
+
+class Square {
+  constructor(x, y, w){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+  }
+  display(){
+    square(this.x, this.y, this.w);
+      arc(this.x + this.w, 
+        this.y + this.w, 
+        this.w * 2, 
+        this.w * 2, 
+        PI, PI + HALF_PI);
+  }
+}
